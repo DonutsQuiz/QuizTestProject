45cdd4d4f866c4546b80f536cf8b30a3dd683da0
TakedaNaomichi <takeda.naomichi@donuts.ne.jp>
2023-03-30 19:00:17 +0900

    ステータス画面の調整

--- assets/Script/Game/Modal/StatusModal.ts
+++ assets/Script/Game/Modal/StatusModal.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, UIOpacity, UITransform, Vec3, lerp, Button } from 'cc';
+import { _decorator, Component, Node, UIOpacity, UITransform, Vec3, lerp, Button, Vec2, Sprite, Prefab, instantiate, getRenderArea, VerticalTextAlignment } from 'cc';
 import { GameManager } from '../Manager/GameManager';
 import { QuizModalManager } from '../Manager/QuizModalManager';
 const { ccclass, property } = _decorator;
@@ -7,8 +7,6 @@ const { ccclass, property } = _decorator;
 export class StatusModal extends Component {
 
     @property(Node)
-    private pointList : Array<Node> = new Array<Node>();
-    @property(Node)
     private player: Node = null
     @property(UITransform)
     private content : UITransform = null;
@@ -16,10 +14,21 @@ export class StatusModal extends Component {
     private onemoreButton : Button = null;
     @property(Button)
     private debugJumpButton : Button = null;
+    @property(Prefab)
+    private checkPointPrefab : Prefab = null;
+    @property(Node)
+    private pointParentNode : Node = null;
+    @property(Number)
+    private generatePointNum : number = 0;
+    @property(Vec2)
+    private basePoint : Vec2 = null;
 
     private JUMP_ANIME_TIME = 2;
     private jumpAnimationTime = -1;
+    private INTERVAL_HORIZON : number = 80;
+    private INTERVAL_VERTICAL : number = 32;
     private CONTENT : number = 0;
+    private checkPointList : Array<Node> = new Array<Node>();
 
     public Constructor(){
         this.debugJumpButton.node.on(Button.EventType.CLICK, this.ClickDebugJump, this);
@@ -27,13 +36,15 @@ export class StatusModal extends Component {
             QuizModalManager.Instance().ChangeModal('Genre');
             GameManager.Instance().SetParticipantActive(true);},
             this);
+
+        this.Generate();
     }
 
     OnUpdate(deltaTime : number){
         if(this.jumpAnimationTime >= 0){
             this.jumpAnimationTime -= deltaTime;
             let timerate = 1 - (this.jumpAnimationTime / this.JUMP_ANIME_TIME);
-            this.player.position = this.pointList[GameManager.Instance().GetGameInfo().status].position.lerp(this.pointList[GameManager.Instance().GetGameInfo().status + 1].position, timerate);
+            this.player.position = this.checkPointList[GameManager.Instance().GetGameInfo().status].position.lerp(this.checkPointList[GameManager.Instance().GetGameInfo().status + 1].position, timerate);
             this.player.position = new Vec3(this.player.position.x, this.player.position.y + Math.sin(timerate * 3.14) * 50, this.player.position.z);
 
             // contentをズラす処理
@@ -51,6 +62,32 @@ export class StatusModal extends Component {
 
     private ClickDebugJump(){
         this.jumpAnimationTime = this.JUMP_ANIME_TIME;
+    }
+
+    public Generate(){
+        if(this.checkPointList.length < this.generatePointNum){
+            for(var i = this.checkPointList.length; i < this.generatePointNum; i++){
+
+                var temp = instantiate(this.checkPointPrefab);
+                temp.setParent(this.pointParentNode);
+                let tempx  : number = 0;
+                if(i % 4 === 0){
+                    tempx = this.basePoint.x + this.INTERVAL_HORIZON * 0;
+                }
+                else if(i % 2 === 1){
+                    tempx = this.basePoint.x + this.INTERVAL_HORIZON * 1;
+                }
+                else if(i % 4 === 2){
+                    tempx = this.basePoint.x + this.INTERVAL_HORIZON * 2;
+                }
+                temp.position = new Vec3(
+                    tempx,
+                    this.basePoint.y + this.INTERVAL_VERTICAL * i,
+                    0);
+                this.checkPointList.push(temp);
+            }
+        }
+
     }
 
 
